0ef5c0ed38820cdd29d1d8dd92e4d198e08a3485
Inverle <inverle@proton.me>
2025-10-12 16:59:35 +0200

    fsdfdsfsdfsdf

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-GITHUB_TOKEN="$(grep -oP 'basic \K[^$]*' .git/config | base64 -d | sed 's/x-access-token://')" gh pr edit 3 --add-label "documentation"
+# GITHUB_TOKEN="$(grep -oP 'basic \K[^$]*' .git/config | base64 -d | sed 's/x-access-token://')" gh pr edit 3 --add-label "documentation"
